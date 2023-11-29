78c450449649d92e2cca7fa02f8f612ba72eccc2
GOUTHAMI SUVARNA <76654382+Gomigoku@users.noreply.github.com>
2023-11-29 15:13:09 +0530

    Create one.js

---
+++ scenario based questions/one.js
@@ -0,0 +1,8 @@
+console.log(2 + true);
+
+output:
+3
+
+reason:
+whenever + operator is found along with the number it tries to convert the next operand to the same type that is
+here it tries to convert true to it's numeric equivalent that is 1.
