f474a7a9c14aaeac0e8a60e5461d1496d48f627e
GOUTHAMI SUVARNA <76654382+Gomigoku@users.noreply.github.com>
2023-11-29 22:39:44 +0530

    Create five.js

---
+++ scenario based questions/five.js
@@ -0,0 +1,8 @@
+console.log(null == undefined);
+
+output:
+true
+
+reason:
+In JavaScript, null and undefined are considered equal only when
+using loose equality (==). Therefore, null == undefined evaluates to true.
